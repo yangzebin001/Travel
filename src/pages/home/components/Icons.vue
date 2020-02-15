cc1d12ccf4015eeb6bc7a6df4c0232bc12127a0b
yangzebin <405678856@qq.com>
2020-02-15 22:24:22 +0800

    add HomeIcons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,129 @@
+<!--
+ * @Date: 2020-02-15 21:38:16
+ * @LastEditors: BeckoninGshy
+ * @LastEditTime: 2020-02-15 21:38:17
+ -->
+<template>
+  <div class="content">
+    <swiper>
+      <swiper-slide
+        v-for="(page,index) in pages"
+        :key="index"
+      >
+        <div class="icons">
+          <div class="icon"
+            v-for="item in page"
+            :key="item.id"
+          >
+            <div class="icon-content">
+              <img class="icon-img" :src="item.imgUrl">
+            </div>
+            <p class="icon-desc">{{item.desc}}</p>
+          </div>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/50/26ffa31b56646402.png',
+        desc: '名胜古迹'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '名胜古迹'
+      }, {
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/50/26ffa31b56646402.png',
+        desc: '景点门票'
+      }, {
+        id: '0006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/50/26ffa31b56646402.png',
+        desc: '景点门票'
+      }, {
+        id: '0008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '名胜古迹'
+      }, {
+        id: '0009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0010',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '名胜古迹'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+  @import "~styles/mixins.styl"
+  .icons
+    overflow hidden
+    height 0
+    padding-bottom 50%
+    .icon
+      position relative
+      overflow hidden
+      float left
+      width 25%
+      height 0
+      padding-bottom 25%
+      .icon-content
+        position absolute
+        left 0
+        top 0
+        right 0
+        bottom .44rem
+        box-sizing border-box
+        padding .1rem
+        width 100%
+        .icon-img
+          display block
+          margin 0 auto
+          height 100%
+      .icon-desc
+        position absolute
+        left 0
+        bottom 0
+        right 0
+        height .44rem
+        line-height .44rem
+        color $darkTextColor
+        text-align center
+        ellipsis()
+</style>
