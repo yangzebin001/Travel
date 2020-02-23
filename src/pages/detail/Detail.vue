f1e38d0a1f60a539e7b41ef156af4be0afc105b3
yangzebin <405678856@qq.com>
2020-02-23 20:26:42 +0800

    a

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,19 +1,22 @@
 <!--
  * @Date: 2020-02-21 20:01:10
  * @LastEditors: BeckoninGshy
- * @LastEditTime: 2020-02-21 20:10:59
+ * @LastEditTime: 2020-02-23 20:23:48
  -->
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-list></detail-list>
   </div>
 </template>
 <script>
 import DetailBanner from './components/Banner'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailList
   }
 }
 </script>
---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,16 @@
+<!--
+ * @Date: 2020-02-23 20:19:27
+ * @LastEditors: BeckoninGshy
+ * @LastEditTime: 2020-02-23 20:23:58
+ -->
+<template>
+  <div>aaa</div>
+</template>
+<script>
+export default {
+  name: 'DetailList'
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
