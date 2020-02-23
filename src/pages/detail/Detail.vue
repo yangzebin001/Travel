3f676aff3d7c846254f2991045375825e30a69e0
yangzebin <405678856@qq.com>
2020-02-23 19:46:35 +0800

    finshed detail-header

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,22 +1,27 @@
 <!--
  * @Date: 2020-02-21 20:01:10
  * @LastEditors: BeckoninGshy
- * @LastEditTime: 2020-02-21 20:10:59
+ * @LastEditTime: 2020-02-23 19:24:59
  -->
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="content"></div>
   </div>
 </template>
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 <style lang="stylus" scoped>
-
+  .content
+    height 100rem
 </style>
